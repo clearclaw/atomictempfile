54783a576468c95a3455caba3da834e0cb0b8e5a
J C Lawrence <claw@kanga.nu>
2016-05-06 20:09:07 -0700

    Cleaner

--- atomictempfile/atomictempfile.py
+++ atomictempfile/atomictempfile.py
@@ -19,12 +19,12 @@ class AtomicTempFile (object): # pylint: disable=R0903
 
   @logtool.log_call
   def __init__(self, final_path, **kwargs):
+    self.final_path = str (final_path) # str cast for broken pathlib
     tmpfile_dir = kwargs.pop ('dir', None)
     if tmpfile_dir is None:
-      tmpfile_dir = os.path.dirname (str (final_path)) # str cast for broken pathlib
+      tmpfile_dir = os.path.dirname (self.final_path)
     self.tmpfile = tempfile.NamedTemporaryFile (dir = tmpfile_dir,
                                                 delete = False, **kwargs)
-    self.final_path = final_path
 
   @logtool.log_call
   def __getattr__ (self, attr):
@@ -40,4 +40,4 @@ class AtomicTempFile (object): # pylint: disable=R0903
     if exc_type is None:
       self.tmpfile.delete = False
       self.tmpfile.__exit__ (exc_type, exc_val, exc_tb)
-      os.rename (self.tmpfile.name, str (self.final_path)) # Grrr pathlib
+      os.rename (self.tmpfile.name, self.final_path)
